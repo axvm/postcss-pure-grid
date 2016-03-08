020625e64527c847c985080b60517abab7853b35
axvm <axvmindaweb@gmail.com>
2016-03-08 15:08:45 +0300

    builds clean pkg without side dependencies

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,42 @@
 import gulp from 'gulp';
 
-//dunno why i did it. maybe later?
+gulp.task('clean', () => {
+  let del = require('del');
+  return del(['build/'])
+});
+
+gulp.task('build:lib', ['clean'], () => {
+  let babel = require('gulp-babel');
+  return gulp.src('postcss-pure-grid.js')
+    .pipe(babel())
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('build:docs', ['clean'], () => {
+    let ignore = require('fs').readFileSync('.npmignore').toString()
+        .trim().split(/\n+/)
+        .concat(['.npmignore', 'index.js', 'package.json',
+                 'lib/*', 'test/*', 'node_modules/**/*'])
+        .map( i => '!' + i );
+    return gulp.src(['**/*'].concat(ignore))
+        .pipe(gulp.dest('build'));
+});
+
+
+gulp.task('build:package', ['clean'], () => {
+  let editor = require('gulp-json-editor');
+  return gulp.src('./package.json')
+    .pipe(editor(json => {
+      json.main = 'postcss-pure-grid';
+      for ( let i in json.dependencies ) {
+        if ( /^babel-/.test(i) ) {
+          json.devDependencies[i] = json.dependencies[i];
+          delete json.dependencies[i];
+        }
+       }
+       return json;
+    }))
+    .pipe(gulp.dest('build'))
+});
+
+gulp.task('build', ['build:lib', 'build:docs', 'build:package']);
